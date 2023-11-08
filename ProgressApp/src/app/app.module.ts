f642c9b83245f1308ae7feb2b7d373038d3e7c3e
Mads Baadsmand <baadsmand@gmail.com>
2023-11-08 16:31:44 +0100

    ProgressApp: add navbar and create-project component

--- ProgressApp/src/app/app.module.ts
+++ ProgressApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateProjectComponent } from './components/create-project/create-project.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateProjectComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
