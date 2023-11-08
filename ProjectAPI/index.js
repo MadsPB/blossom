4b3f48996e1996ee89ea1553eab04c72e0882f2a
Mads Baadsmand <baadsmand@gmail.com>
2023-11-08 22:22:25 +0100

    ProjectApi: add cors

--- ProjectAPI/index.js
+++ ProjectAPI/index.js
@@ -2,8 +2,18 @@ import Koa from 'koa';
 const app = new Koa();
 import { bodyParser } from "@koa/bodyparser";
 import router from './router.js';
+import cors from 'koa2-cors'
 const port = 3001;
 
+const options = {
+  origin: '*'
+};
+app.use(cors())
+app.use(async (ctx, next) => {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  await next();
+});
 app.use(bodyParser())
 app.use(router.routes());
 
