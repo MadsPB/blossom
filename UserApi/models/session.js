e7f905203a7fca66ca1ae614e74585c5066e7da0
Mads Baadsmand <baadsmand@gmail.com>
2023-11-10 17:20:19 +0100

    UserApi: adjust session length to three minutes

--- UserApi/models/session.js
+++ UserApi/models/session.js
@@ -1,7 +1,7 @@
 import redisDb from "../infrastructure/redisdb.js";
 import { v4 as uuidv4 } from 'uuid';
 
-const EXPIRATION = 60*1;
+const EXPIRATION = 60*3;
 
 export async function createSession(userId)
 {
